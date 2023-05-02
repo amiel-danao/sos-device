dcecca97b66b6d093767535579721a772cb9f976
unknown <amielrenaissance4@gmail.com>
2023-05-02 12:03:25 +0800

    added buttons functionality for flashlight, alarm, taser

--- sos_device.ino
+++ sos_device.ino
@@ -1,100 +1,125 @@
 #include <SoftwareSerial.h>
 #include <TinyGPSPlus.h>
-#include <pdulib.h>
+#include "OneButton.h"
 // #include <CStringBuilder.h>
 
 #define rxPin 6
 #define txPin 5
+#define BUTTON1_PIN 2
+#define BUTTON2_PIN 8
+#define FLASHLIGHT_PIN 7
+#define ALARM_PIN 9
 
-SoftwareSerial A9modem(rxPin, txPin);  // Pins D7 Rx and D8 Tx are used as used as software serial pins
+SoftwareSerial A9modem(rxPin, txPin);  // Pins D6 Rx and D5 Tx are used as used as software serial pins
 TinyGPSPlus gps;
 
-
-const String defaultMessage = "Please help me, this is my current location www.google.com/maps/dir/14.32,120.98/";
-// char incomingData[128];  // For storing incoming serial data
-// CStringBuilder sb(incomingData, sizeof(incomingData));
+const String defaultMessage = "Please help me, I'm currently located at ";
 String incomingData;
-const char *Target = "+639973995372"; 
-#define BUFFER_LENGTH 100
-PDU mypdu = PDU(BUFFER_LENGTH);
-char temp[30];
+String tempData;
+const char sample[] = "Please help me this is an emergency! I'm located at www.google.com/maps/place/14.32,120.98";
 
+OneButton button1(BUTTON1_PIN, false);
+OneButton button2(BUTTON2_PIN, false);
+
+int flashLightState = LOW;
+int alarmState = LOW;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);  // Use builtin LED for correct GPS status
-  
+
   Serial.begin(115200);   // Baud rate for serial monitor
   A9modem.begin(115200);  // Baud rate for GSM shield
 
-  Serial.println("started");
-  A9modem.println("AT+GPS=1");
-  delay(1000);
-  A9modem.println("AT+GPSMD=2\r");   // Change to only GPS mode from GPS+BDS, set to 2 to revert to default.
-  delay(1000);
+  pinMode(LED_BUILTIN, OUTPUT);  // Use builtin LED for correct GPS status
+  pinMode(FLASHLIGHT_PIN, OUTPUT);
+  pinMode(ALARM_PIN, OUTPUT);
+  button1.attachClick(toggleFlashLight);
+  button2.attachClick(toggleAlarm);
+  button2.attachLongPressStop(sendLocation);
+
+  // Serial.println("started");
+  // A9modem.println("AT+GPS=1");
+  // delay(1000);
+  // A9modem.println("AT+GPSMD=2\r");   // Change to only GPS mode from GPS+BDS, set to 2 to revert to default.
+  // delay(1000);
   // A9modem.println("AT+GPSRD=5\r");
 
-  delay(1000);
-  // Set SMS mode to text mode
+  // delay(1000);
+  // // Set SMS mode to text mode
   // A9modem.print("AT+CMGF=1\r");
   // delay(1000);
   
-  // Set GSM module to TP show the output on serial out
-  A9modem.print("AT+CNMI=2,2,0,0,0\r"); 
-  delay(1000);
+  // // Set GSM module to TP show the output on serial out
+  // A9modem.print("AT+CNMI=2,2,0,0,0\r"); 
+  // delay(1000);
 
-  A9modem.println("AT+CREG=2\r");
-  delay(6000);
+  // A9modem.println("AT+CREG=2\r");
+  // delay(6000);
 
-  //A9modem.print("AT+CREG?\r");
-  A9modem.println("AT+CGATT=1\r");
-  delay(6000);
+  // A9modem.println("AT+CGATT=1\r");
+  // delay(6000);
 
-  A9modem.println("AT+CGDCONT=1,\"IP\",\"WWW\"\r");
-  delay(6000);
+  // A9modem.println("AT+CGDCONT=1,\"IP\",\"internet\"\r");
+  // delay(6000);
 
-  // A9modem.println("AT+LOCATION=1\r");
-  A9modem.println("AT+CGACT=1,1\r");
-  delay(6000);
+  // A9modem.println("AT+CGACT=1,1\r");
+  // delay(6000);
   Serial.println("Done initialization");
-
-  // SendSMS();
 }
 
-void loop(){
-  if (A9modem.available() > 0)
-  {
-    String tokens = A9modem.readString();
-    Serial.println(tokens);
-    A9modem.println("AT+CMGF=1\r");
-    delay(1000);
-    A9modem.println("AT+CNMI=2,2,0,0,0\r");
-    delay(1000);
-    // char sample[] = "https://goo.gl/maps/eNVqDopP9cVBC4tM6";
-    char sample[] = "Please help me this is an emergency!";
-    // char sample[] = "LOL Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s";
-    
-    // int len = mypdu.encodePDU(Target, sample);
-    // sprintf(temp,"AT+CMGS=%d\r",len);
-    // A9modem.print(temp);
-    A9modem.print("AT+CMGS=\"+639954261220\"\r");  // Replace your mobile number here
-    delay(1000);
+void toggleFlashLight(){
+  flashLightState = !flashLightState;
+  Serial.println("Flashlight toggled : " + String(flashLightState));
+  digitalWrite(FLASHLIGHT_PIN, flashLightState);
+}
 
-    A9modem.print(sample);
-    // A9modem.print("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum");
-    
-    
-    // A9modem.print(mypdu.getSMS());
-    // A9modem.print(incomingData);
-    delay(5000);
-    // delay(200);
-    A9modem.write(0x1A);
-    // A9modem.println((char)26);          // ASCII code of CTRL+Z
-    delay(10000);
+void toggleAlarm(){
+  alarmState = !alarmState;
+  Serial.println("Alarm toggled : " + String(alarmState));
+  if(alarmState){
+    for (float f=3000;f>40;f=f*0.93){
+      tone(ALARM_PIN, f);
+      delay(10);
+    }
+  }
+  else{
+    noTone(ALARM_PIN);
   }
 }
 
-void loop2() {
+void loop2(){
+  
+
+  while (A9modem.available() > 0)
+  {
+    Serial.print((char)A9modem.read());
+    // String tokens = A9modem.readString();    
+  }
+}
+
+void sendLocation(){
+  if(incomingData.length() > 0) {  // If the location string is correct send SMS
+    // if (isdigit(gpsData.charAt(0))) {           // Check if the stream starts with a number
+      SendSMS();
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(300);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(300);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(300);
+      digitalWrite(LED_BUILTIN, LOW);  
+      incomingData = "";
+      tempData = "";
+    // } 
+  }
+}
+
+
+void loop() {
+
+  button1.tick();
+  button2.tick();
+
+  delay(10);
 
   if (A9modem.available() > 0)
   {
@@ -106,7 +131,6 @@ void loop2() {
       float latitude = 0;
       float longitude = 0;
         uint8_t token_idx = 0;
-        String tempData;
         while (get_token(tokens, token, token_idx, ','))
         {
           // Serial.print("Token[");
@@ -122,7 +146,7 @@ void loop2() {
             else{
               latitude = GpsToDecimalDegrees(token.c_str(), 'S');
             }
-            tempData += String(latitude);
+            tempData += String(latitude, 6);
           }
           if(token_idx == 4){
             tempData += ",";
@@ -133,7 +157,7 @@ void loop2() {
             else{
               longitude = GpsToDecimalDegrees(token.c_str(), 'E');
             }
-            tempData += String(longitude);
+            tempData += String(longitude, 6);
           }
           token_idx++;
         }
@@ -148,7 +172,6 @@ void loop2() {
           // sb.print(F("/"));
           incomingData = "www.google.com/maps/dir/";
           incomingData += tempData;  // Create the SMS string
-          incomingData += "/";
         }
       Serial.println(incomingData); 
     }
@@ -158,19 +181,7 @@ void loop2() {
     // delay(500); 
   }
 
-  if(incomingData.length() > 0) {  // If the location string is correct send SMS
-    // if (isdigit(gpsData.charAt(0))) {           // Check if the stream starts with a number
-      SendSMS();
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(300);
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(300);
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(300);
-      digitalWrite(LED_BUILTIN, LOW);  
-      incomingData = "";
-    // } 
-  }
+  
 }
 
 
@@ -233,7 +244,7 @@ void SendSMS()
   delay(1000);
 
   // A9modem.print("LOL");
-  A9modem.print(defaultMessage);
+  A9modem.print(defaultMessage + tempData);
   // A9modem.print(incomingData);
   // delay(5000);
   // delay(1000);
@@ -241,6 +252,8 @@ void SendSMS()
   // A9modem.println((char)26);          // ASCII code of CTRL+Z
   delay(5000);
   Serial.println("message was sent");
+
+  
   // delay(1000);
   // A9modem.println("ATD+639954261220");
 }
